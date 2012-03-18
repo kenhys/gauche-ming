c636ab9825da81fafc26a04862dbcaee864a4771
HAYASHI Kentaro <kenhys@gmail.com>
2012-03-18 23:22:14 +0900

    add swf-* types.

--- src/gauche_ming.c
+++ src/gauche_ming.c
@@ -18,10 +18,24 @@ ScmObj test_gauche_ming(void)
  * Module initialization function.
  */
 extern void Scm_Init_gauche_minglib(ScmModule*);
-extern void Scm_Init_ming_swfmovie(ScmModule*);
-extern void Scm_Init_ming_swfshape(ScmModule*);
-extern void Scm_Init_ming_swftext(ScmModule*);
+extern void Scm_Init_ming_swfaction(ScmModule*);
+extern void Scm_Init_ming_swfbutton(ScmModule*);
+extern void Scm_Init_ming_swfdisplayitem(ScmModule*);
+extern void Scm_Init_ming_swffill(ScmModule*);
+extern void Scm_Init_ming_swffillstyle(ScmModule*);
+extern void Scm_Init_ming_swffilter(ScmModule*);
 extern void Scm_Init_ming_swffont(ScmModule*);
+extern void Scm_Init_ming_swflinestyle(ScmModule*);
+extern void Scm_Init_ming_swfmorph(ScmModule*);
+extern void Scm_Init_ming_swfmovie(ScmModule*);
+extern void Scm_Init_ming_swfmovieclip(ScmModule*);
+extern void Scm_Init_ming_swfposition(ScmModule*);
+extern void Scm_Init_ming_swfshape(ScmModule*);
+extern void Scm_Init_ming_swfsound(ScmModule*);
+extern void Scm_Init_ming_swfsprite(ScmModule*);
+extern void Scm_Init_ming_swftext(ScmModule*);
+extern void Scm_Init_ming_swftextfield(ScmModule*);
+extern void Scm_Init_ming_swfvideo(ScmModule*);
 
 void Scm_Init_gauche_ming(void)
 {
@@ -35,8 +49,22 @@ void Scm_Init_gauche_ming(void)
 
     /* Register stub-generated procedures */
     Scm_Init_gauche_minglib(mod);
-    Scm_Init_ming_swfmovie(mod);
-    Scm_Init_ming_swfshape(mod);
-    Scm_Init_ming_swftext(mod);
+    Scm_Init_ming_swfaction(mod);
+    Scm_Init_ming_swfbutton(mod);
+    Scm_Init_ming_swfdisplayitem(mod);
+    Scm_Init_ming_swffill(mod);
+    Scm_Init_ming_swffillstyle(mod);
+    Scm_Init_ming_swffilter(mod);
     Scm_Init_ming_swffont(mod);
+    Scm_Init_ming_swflinestyle(mod);
+    Scm_Init_ming_swfmorph(mod);
+    Scm_Init_ming_swfmovie(mod);
+    Scm_Init_ming_swfmovieclip(mod);
+    Scm_Init_ming_swfposition(mod);
+    Scm_Init_ming_swfshape(mod);
+    Scm_Init_ming_swfsound(mod);
+    Scm_Init_ming_swfsprite(mod);
+    Scm_Init_ming_swftext(mod);
+    Scm_Init_ming_swftextfield(mod);
+    Scm_Init_ming_swfvideo(mod);
 }
--- src/gauche_ming.h
+++ src/gauche_ming.h
@@ -23,6 +23,76 @@ extern ScmObj test_gauche_ming(void);
 #define SCM_BYTE(obj) ((unsigned char)(obj))
 #define SCM_BYTE_P(obj) true
 
+typedef struct ScmSWFBitmapRec {
+SCM_HEADER;
+SWFBitmap h;
+} ScmSWFBitmap;
+
+SCM_CLASS_DECL(Scm_SWFBitmapClass);
+#define SCM_CLASS_SWF_BITMAP (&Scm_SWFBitmapClass)
+#define SCM_SWF_BITMAP(obj) ((ScmSWFBitmap*)(obj))
+#define SCM_SWF_BITMAP_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_BITMAP))
+
+typedef struct ScmSWFCharacterRec {
+SCM_HEADER;
+SWFCharacter h;
+} ScmSWFCharacter;
+
+SCM_CLASS_DECL(Scm_SWFCharacterClass);
+#define SCM_CLASS_SWF_CHARACTER (&Scm_SWFCharacterClass)
+#define SCM_SWF_CHARACTER(obj) ((ScmSWFCharacter*)(obj))
+#define SCM_SWF_CHARACTER_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_CHARACTER))
+
+typedef struct ScmSWFDisplayItemRec {
+SCM_HEADER;
+SWFDisplayItem h;
+} ScmSWFDisplayItem;
+
+SCM_CLASS_DECL(Scm_SWFDisplayItemClass);
+#define SCM_CLASS_SWF_DISPLAY_ITEM (&Scm_SWFDisplayItemClass)
+#define SCM_SWF_DISPLAY_ITEM(obj) ((ScmSWFDisplayItem*)(obj))
+#define SCM_SWF_DISPLAY_ITEM_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_DISPLAY_ITEM))
+
+typedef struct ScmSWFFontRec {
+SCM_HEADER;
+SWFFont h;
+} ScmSWFFont;
+
+SCM_CLASS_DECL(Scm_SWFFontClass);
+#define SCM_CLASS_SWF_FONT (&Scm_SWFFontClass)
+#define SCM_SWF_FONT(obj) ((ScmSWFFont*)(obj))
+#define SCM_SWF_FONT_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_FONT))
+
+typedef struct ScmSWFFillRec {
+SCM_HEADER;
+SWFFill h;
+} ScmSWFFill;
+
+SCM_CLASS_DECL(Scm_SWFFillClass);
+#define SCM_CLASS_SWF_FILL (&Scm_SWFFillStyleClass)
+#define SCM_SWF_FILL(obj) ((ScmSWFFill*)(obj))
+#define SCM_SWF_FILL_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_FILL))
+
+typedef struct ScmSWFFillStyleRec {
+SCM_HEADER;
+SWFFillStyle h;
+} ScmSWFFillStyle;
+
+SCM_CLASS_DECL(Scm_SWFFillStyleClass);
+#define SCM_CLASS_SWF_FILL_STYLE (&Scm_SWFFillStyleClass)
+#define SCM_SWF_FILL_STYLE(obj) ((ScmSWFFillStyle*)(obj))
+#define SCM_SWF_FILL_STYLE_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_FILL_STYLE))
+
+typedef struct ScmSWFGradientRec {
+SCM_HEADER;
+SWFGradient h;
+} ScmSWFGradient;
+
+SCM_CLASS_DECL(Scm_SWFGradientClass);
+#define SCM_CLASS_SWF_GRADIENT (&Scm_SWFGradientClass)
+#define SCM_SWF_GRADIENT(obj) ((ScmSWFGradient*)(obj))
+#define SCM_SWF_GRADIENT_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_GRADIENT))
+
 typedef struct ScmSWFMovieRec {
 SCM_HEADER;
 SWFMovie h;
@@ -32,6 +102,16 @@ SCM_CLASS_DECL(Scm_SWFMovieClass);
 #define SCM_CLASS_SWF_MOVIE (&Scm_SWFMovieClass)
 #define SCM_SWF_MOVIE(obj) ((ScmSWFMovie*)(obj))
 #define SCM_SWF_MOVIE_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_MOVIE))
+
+typedef struct ScmSWFMovieBlockTypeRec {
+SCM_HEADER;
+SWFMovieBlockType *h;
+} ScmSWFMovieBlockType;
+
+SCM_CLASS_DECL(Scm_SWFMovieBlockTypeClass);
+#define SCM_CLASS_SWF_MOVIE_BLOCK_TYPE (&Scm_SWFMovieBlockTypeClass)
+#define SCM_SWF_MOVIE_BLOCK_TYPE(obj) ((ScmSWFMovieBlockType*)(obj))
+#define SCM_SWF_MOVIE_BLOCK_TYPE_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_MOVIE_BLOCK_TYPE))
 
 typedef struct ScmSWFShapeRec {
 SCM_HEADER;
@@ -53,35 +133,28 @@ SCM_CLASS_DECL(Scm_SWFTextClass);
 #define SCM_SWF_TEXT(obj) ((ScmSWFText*)(obj))
 #define SCM_SWF_TEXT_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_TEXT))
 
-typedef struct ScmSWFFontRec {
+
+
+typedef struct ScmSWFLineStyleRec {
 SCM_HEADER;
-SWFFont h;
-} ScmSWFFont;
+SWFLineStyle h;
+} ScmSWFLineStyle;
 
-SCM_CLASS_DECL(Scm_SWFFontClass);
-#define SCM_CLASS_SWF_FONT (&Scm_SWFFontClass)
-#define SCM_SWF_FONT(obj) ((ScmSWFFont*)(obj))
-#define SCM_SWF_FONT_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_FONT))
+SCM_CLASS_DECL(Scm_SWFLineStyleClass);
+#define SCM_CLASS_SWF_LINE_STYLE (&Scm_SWFLineStyleClass)
+#define SCM_SWF_LINE_STYLE(obj) ((ScmSWFLineStyle*)(obj))
+#define SCM_SWF_LINE_STYLE_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_LINE_STYLE))
 
-typedef struct ScmSWFDisplayItemRec {
+typedef struct ScmSWFMatrixRec {
 SCM_HEADER;
-SWFDisplayItem h;
-} ScmSWFDisplayItem;
+SWFMatrix h;
+} ScmSWFMatrix;
 
-SCM_CLASS_DECL(Scm_SWFDisplayItemClass);
-#define SCM_CLASS_SWF_DISPLAY_ITEM (&Scm_SWFDisplayItemClass)
-#define SCM_SWF_DISPLAY_ITEM(obj) ((ScmSWFDisplayItem*)(obj))
-#define SCM_SWF_DISPLAY_ITEM_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_DISPLAY_ITEM))
+SCM_CLASS_DECL(Scm_SWFMatrixClass);
+#define SCM_CLASS_SWF_MATRIX (&Scm_SWFMatrixClass)
+#define SCM_SWF_MATRIX(obj) ((ScmSWFMatrix*)(obj))
+#define SCM_SWF_MATRIX_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_MATRIX))
 
-typedef struct ScmSWFCharacterRec {
-SCM_HEADER;
-SWFCharacter h;
-} ScmSWFCharacter;
-
-SCM_CLASS_DECL(Scm_SWFCharacterClass);
-#define SCM_CLASS_SWF_CHARACTER (&Scm_SWFCharacterClass)
-#define SCM_SWF_CHARACTER(obj) ((ScmSWFCharacter*)(obj))
-#define SCM_SWF_CHARACTER_P(obj) (SCM_XTYPEP(obj, SCM_CLASS_SWF_CHARACTER))
 
 
 
