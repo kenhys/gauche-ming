944550254f52efdbc20809745d783b2ac7729f26
HAYASHI Kentaro <kenhys@gmail.com>
2012-03-18 23:20:19 +0900

    add swf-movie- alias.

--- lib/ming/swfmovie.scm
+++ lib/ming/swfmovie.scm
@@ -32,6 +32,29 @@
 (define-method number-of-frames! ((self <swf-movie>) frames)
   (swf-movie-set-number-of-frames self frames))
 
+(define-method background! 
+  ((self <swf-movie>) r g b)
+  (swf-movie-set-background self r g b))
+
+(define-method add-internal
+  ((self <swf-movie>) ublock)
+  (swf-movie-add-internal self ublock))
+
+(define-method add
+  ((self <swf-movie>) ublock)
+  (swf-movie-add self ublock))
+
+(define-method next-frame ((self <swf-movie>))
+  (swf-movie-next-frame self))
+
+(define-method label-frame ((self <swf-movie>) label)
+  (swf-movie-label-frame self label))
+
+(define-method name-anchor ((self <swf-movie>) label)
+  (swf-movie-name-anchor self label))
+
+(define-method save ((self <swf-movie>) filename)
+  (swf-movie-save self filename))
 
 ;;
 ;; Epilogue
